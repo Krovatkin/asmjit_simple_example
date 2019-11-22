c11466930aeaa941fb4edfbaefd4cc7678fabd7d
unknown <Administrator@VM-ek2sma8wzu.TheFacebook.com>
2019-11-22 11:34:23 -0800

    switch to packed

--- main.cpp
+++ main.cpp
@@ -35,7 +35,8 @@ void generateLoopNested(int dimensions[], int rank) {
       int*,
       int*>());
 
-    cc.int3();
+
+	cc.int3();
     Label L_Loop = cc.newLabel();           // Start of the loop.
     Label L_Exit = cc.newLabel();           // Used to exit early.
 
@@ -51,7 +52,7 @@ void generateLoopNested(int dimensions[], int rank) {
     cc.setArg(3, starts);
     cc.setArg(4, ends);
 
-    //cc.int3();
+    //
 
     std::vector<Label> loop_exits;
     std::vector<Label> loop_starts;
@@ -109,7 +110,8 @@ void generateLoopNested(int dimensions[], int rank) {
       cc.imul(tmp, static_cast<int>(strides.at(i)));
       cc.add(start, tmp);
     }
-
+	// real offset in bytes
+	cc.shl(start, 2);
     //FuncCallNode* call_print = cc.call(reinterpret_cast<uint64_t>(&print_offset), FuncSignatureT<void, void*, int>());
     //call_print->setArg(0, Imm(reinterpret_cast<uint64_t>(offset_str)));
     //call_print->setArg(1, start);
@@ -119,22 +121,18 @@ void generateLoopNested(int dimensions[], int rank) {
     x86::Gp cnt = cc.newGpd("inner");
     cc.mov(cnt, x86::dword_ptr(ends, (rank - 1) << 2));
     cc.sub(cnt, x86::dword_ptr(starts, (rank - 1) << 2));
-
+	
     // loop body
     cc.bind(L_Loop);                        // Bind the beginning of the loop here.
-
-    auto xmm_a = cc.newXmmSs("a");
-    cc.movss(xmm_a, x86::dword_ptr(src1, start, 2));
-    auto xmm_b = cc.newXmmSs("b");
-    cc.movss(xmm_b, x86::dword_ptr(src2, start, 2));
-    cc.addss(xmm_a, xmm_b);
-    cc.movss(x86::dword_ptr(dst, start, 2), xmm_a);
+    auto xmm_a = cc.newXmmPs("a");
+    cc.movups(xmm_a, x86::dword_ptr(src1, start));
+    auto xmm_b = cc.newXmmPs("b");
+    cc.movups(xmm_b, x86::dword_ptr(src2, start));
+    cc.addps(xmm_a, xmm_b);
+    cc.movups(x86::dword_ptr(dst, start), xmm_a);
     
-    cc.add(src1, 4);
-    cc.add(src2, 4);
-    cc.add(dst, 4);
-
-    cc.dec(cnt);   
+	cc.add(start, 16);
+    cc.sub(cnt, 4);   
     cc.jnz(L_Loop);
     cc.bind(L_Exit);
     //compute start index for innermost loop
@@ -184,95 +182,18 @@ void generateLoopNested(int dimensions[], int rank) {
     std::vector<float> b(num_elems, 3.f);
     std::vector<float> c(num_elems, 0.f);
     std::vector<int> starts_v(rank, 0);
-    std::vector<int> ends_v(rank, 2);
+	std::vector<int> ends_v{1,1,16};
     std::cout << "ends_v.data = " << ends_v.data() << " " << ends_v.data()[0] << std::endl;
     add32_2(a.data(), b.data(), c.data(), starts_v.data(), ends_v.data());
     std::cout << "c = ";
     for (int i = 0; i < num_elems; i++) {
       std::cout << " " << c.at(i); 
     }
-    jit.release(add32_2);  
+    jit.release(add32_2);
 }
-
-
-void generateLoopNested() {
-  
-  JitRuntime jit;                         // Runtime specialized for JIT code execution.
-  CodeHolder code;                        // Holds code and relocation information.
-  FileLogger logger(stdout);
-  logger.addFlags(FormatOptions::kFlagMachineCode);
-  code.setLogger(&logger);
-  code.init(jit.codeInfo());              // Initialize to the same arch as JIT runtime.
-  x86::Compiler cc(&code);                // Create and attach x86::Compiler to `code`.
-  cc.addFunc(                             // Begin the function of the following signature:
-    FuncSignatureT<void,                  //   Return value - void      (no return value).
-      const float*,
-      const float*,
-      float*,
-      int,
-      int>());
-
-    Label L_Loop = cc.newLabel();           // Start of the loop.
-    Label L_Exit = cc.newLabel();           // Used to exit early.
-
-    x86::Gp src1 = cc.newIntPtr("src1");
-    x86::Gp src2 = cc.newIntPtr("src2");
-    x86::Gp dst = cc.newIntPtr("dst");
-    x86::Gp cnt = cc.newUIntPtr("cnt");
-    x86::Gp offset = cc.newUIntPtr("offset");
-
-    cc.setArg(0, src1);
-    cc.setArg(1, src2);
-    cc.setArg(2, dst);
-    cc.setArg(3, cnt);
-    cc.setArg(4, offset);
-
-    cc.test(cnt, cnt);                      // Early exit if length is zero.
-    cc.jz(L_Exit);
-
-    cc.bind(L_Loop);                        // Bind the beginning of the loop here.
-
-    auto xmm_a = cc.newXmmSs("a");
-    cc.movss(xmm_a, x86::dword_ptr(src1, offset, 2));
-    auto xmm_b = cc.newXmmSs("b");
-    cc.movss(xmm_b, x86::dword_ptr(src2, offset, 2));
-    cc.addss(xmm_a, xmm_b);
-    cc.movss(x86::dword_ptr(dst, offset, 2), xmm_a);
-    
-    cc.add(src1, 4);
-    cc.add(src2, 4);
-    cc.add(dst, 4);
-
-    cc.dec(cnt);   
-    cc.jnz(L_Loop);
-
-    cc.bind(L_Exit);
-    cc.endFunc();  
-    cc.finalize(); 
-
-    const int size = 3;
-    float a [] = {2.f, 3.f, 4.f};
-    float b [] = {1.f, 1.f, 1.f};
-    float c [] = {0.f, 0.f, 0.f};
-
-
-  for (auto i = 0; i < size; i++) {
-    std::cout << " " << c[i];
-  }
-
-  Add32 add32;
-  Error err = jit.add(&add32, &code);  // Add the generated code to the runtime.
-  add32(a,b,c, size - 1, 1);
-  jit.release(add32);   
-
-  for (auto i = 0; i < size; i++) {
-    std::cout << " " << c[i];
-  }
-}
-
 
 int main(int argc, char* argv[]) {
-  int dims [] = {2, 5, 3};
+  int dims [] = {1, 1, 16};
   generateLoopNested(dims, 3);
   return 0;
 }
